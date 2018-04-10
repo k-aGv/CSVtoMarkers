96d79f5d76afe6ec56df3246739e0200e9c5b960
Yannis Menexes <johnmenex@gmail.com>
2018-04-10 12:29:27 +0300

    Notes.cs: Reworked notes logic
    Notes are stored and retrieved as ByteArrays in order to properly handle multi-line notes.

--- GoogleMarkers/Notes.cs
+++ GoogleMarkers/Notes.cs
@@ -15,82 +15,108 @@ namespace GoogleMarkers {
         public Notes(GMapMarker _marker, string _dir) {
             InitializeComponent();
             Marker = _marker;
-            Directory = _dir.Remove(_dir.Length - 9) + "/_notes";
-            LoadNotes();
+            Directory = _dir.Remove(_dir.LastIndexOf('\\'), _dir.Length - _dir.LastIndexOf('\\')) + "/_notes";
+            LoadNotesAsBytes();
             Text = Marker.Tag.ToString();
         }
         private GMapMarker Marker;
         private string Directory;
-        private List<string> ReadNotes() {
 
-            List<string> _whole_file = new List<string>();
-
+        private List<byte[]> ReadNotesAsBytes() {
+            List<byte[]> _list = new List<byte[]>();
             StreamReader _rdr;
             if (File.Exists(Directory)) {
                 _rdr = new StreamReader(Directory);
                 do {
-                    _whole_file.Add(_rdr.ReadLine());
+                    string[] _tmp = _rdr.ReadLine().Split(',');
+                    byte[] _bytes = new byte[_tmp.Length];
+                    for(int i=0; i< _bytes.Length; i++) {
+                        _bytes[i] = Convert.ToByte(_tmp[i]);
+                    }
+                    _list.Add(_bytes);
                 } while (!_rdr.EndOfStream);
                 _rdr.Close();
             }
-            
-            return _whole_file;
+            return _list;
         }
-        private void LoadNotes() {
-            List<string> _whole_file = ReadNotes();
-            if (_whole_file.Count == 0 || _whole_file == null)
+        private void LoadNotesAsBytes() {
+            List<byte[]> _list = ReadNotesAsBytes();
+            if (_list.Count == 0 || _list == null)
                 return;
-            
-            foreach(string _s in _whole_file) {
-                string _tmp = _s.Replace("[", "").Replace("]", "");
-                if (Marker.Tag.ToString() == _tmp.Split('|')[0]) {
-                    tb_notes.Text = _tmp.Split('|')[1];
-                }
-            }
 
+            foreach (byte[] _barray in _list) {
+                string _tmp = Encoding.Unicode.GetString(_barray);
+                
+                _tmp = _tmp.Replace("[", "").Replace("]", "");
+                if (Marker.Tag.ToString() == _tmp.Split('|')[0])
+                    tb_notes.Text = _tmp.Split('|')[1];
+            }
         }
         private void SaveNotes() {
-            List<string> _whole_file = ReadNotes();
+            List<string> _whole_file = new List<string>();
+            List<byte[]> _bytes = ReadNotesAsBytes();
             int _index = -1;
-
+            
             if (File.Exists(Directory))
                 File.Delete(Directory);
+            
+            foreach(byte[] _barray in _bytes) {
+                _whole_file.Add(Encoding.Unicode.GetString(_barray));
+            }
 
-            if (_whole_file.Count != 0 || _whole_file != null) {
+            if (_bytes.Count != 0 || _bytes != null) {
                 int _c = 0;
-                foreach (string _s in _whole_file) {
-                    if (_s != "") {
-
-                        string _tmp = _s.Replace("[", "").Replace("]", "");
-
-                        if (_tmp.Split('|')[0] == Marker.Tag.ToString()) {
-                            _index = _c;
-                        }
-                        _c++;
-                    }
+                foreach(byte[] _barray in _bytes) {
+                    string _tmp = Encoding.Unicode.GetString(_barray);
+                    _tmp = _tmp.Replace("[", "").Replace("]", "");
+                    if(_tmp.Split('|')[0] == Marker.Tag.ToString())
+                        _index = _c;
+                    _c++;
                 }
-                string _line = "";
                 if (_index != -1) {
                     _whole_file[_index] = "[" + Marker.Tag.ToString() + "|" + tb_notes.Text + "]";
                     StreamWriter _wr = new StreamWriter(Directory);
-                    foreach (string _s in _whole_file) {
-                        _wr.WriteLine(_s);
+                    foreach(string _s in _whole_file) {
+                        byte[] _tmp = Encoding.Unicode.GetBytes(_s);
+                        string _line = "";
+                        foreach (byte _b in _tmp) {
+                            _line += _b + ",";
+                        }
+                        _line = _line.Remove(_line.LastIndexOf(','));
+                        _wr.WriteLine(_line);
                     }
                     _wr.Close();
                 }
                 else {
                     StreamWriter _wr = new StreamWriter(Directory);
                     foreach (string _s in _whole_file) {
-                        _wr.WriteLine(_s);
+                        byte[] _tmp = Encoding.Unicode.GetBytes(_s);
+                        string _line = "";
+                        foreach (byte _b in _tmp) {
+                            _line += _b + ",";
+                        }
+                        _line = _line.Remove(_line.LastIndexOf(','));
+                        _wr.WriteLine(_line);
                     }
-                    _line += "[" + Marker.Tag.ToString() + "|" + tb_notes.Text + "]";
-                    _wr.WriteLine(_line);
+                    byte[] _tmpBytes = Encoding.Unicode.GetBytes("[" + Marker.Tag.ToString() + "|" + tb_notes.Text + "]");
+                    string _tmpLine = "";
+                    foreach (byte _b in _tmpBytes) {
+                        _tmpLine += _b + ",";
+                    }
+                    _tmpLine = _tmpLine.Remove(_tmpLine.LastIndexOf(','));
+                    _wr.WriteLine(_tmpLine);
                     _wr.Close();
                 }
             }
             else {
                 StreamWriter _wr = new StreamWriter(Directory);
-                _wr.Write("[" + Marker.Tag.ToString() + "|" + tb_notes.Text + "]");
+                byte[] _tmpBytes = Encoding.Unicode.GetBytes("[" + Marker.Tag.ToString() + "|" + tb_notes.Text + "]");
+                string _tmpLine = "";
+                foreach (byte _b in _tmpBytes) {
+                    _tmpLine += _b + ",";
+                }
+                _tmpLine = _tmpLine.Remove(_tmpLine.LastIndexOf(','));
+                _wr.Write(_tmpLine);
                 _wr.Close();
             }
         }
